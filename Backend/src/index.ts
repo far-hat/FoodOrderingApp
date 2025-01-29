84bcdba4855d503734365d700703071d0e987b0a
far-hat <farhatgulzar96@gmail.com>
2025-01-29 19:57:43 +0530

    backend code for updating user, validation of user input, jwt etc

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -2,6 +2,9 @@ import express,{Request,Response} from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
+
+
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(()=> console.log("Connected to Database"))
 
@@ -10,9 +13,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/test",async(req:Request,res:Response)=> {
-    res.json({message : "Hello!"});
-});
+
+
+app.use("/api/my/user",myUserRoute);
 
 const PORT = 6500;
 
