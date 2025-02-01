890a2a25706d8ca265d239f1893a433922ff2cbb
far-hat <farhatgulzar96@gmail.com>
2025-02-01 20:16:12 +0530

    correction of foldername in path

--- Backend/src/routes/MyUserRoute.js
+++ Backend/src/routes/MyUserRoute.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const MyUserController_1 = __importDefault(require("../components/MyUserController"));
+const MyUserController_1 = __importDefault(require("../controller/MyUserController"));
 const router = express_1.default.Router();
 router.post("/", MyUserController_1.default.createCurrentUser);
 exports.default = router;
