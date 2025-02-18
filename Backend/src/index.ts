969ef8fd246823b8a9306e7a7436542e9a0104d3
far-hat <farhatgulzar96@gmail.com>
2025-02-18 23:09:15 +0530

    Merge branch 'main' of https://github.com/far-hat/FoodOrderingApp

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
 import {v2 as cloudinary} from "cloudinary";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
+import RestaurantRoute from "./routes/RestaurantRoute"
 
 
 
@@ -28,7 +29,8 @@ app.get("/health", async(req: Request, res: Response,next: NextFunction)=>{
 });
 
 app.use("/api/my/user",myUserRoute);
-app.use("/api/my/restaurant",myRestaurantRoute)
+app.use("/api/my/restaurant",myRestaurantRoute);
+app.use("/api/restaurant",RestaurantRoute);
 
 const PORT = 6500;
 
